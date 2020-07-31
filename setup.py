ed448f806ac473fc774cec77bd100b5f770c9135
Felix-Neko <felix-neko@list.ru>
2020-07-31 23:15:34 +0300

    setup.py added...

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='argparse_dataclasses',
+    version='0.0',
+    packages=find_packages(),
+    author="Felix Neko",
+    author_email="felix-neko@list.ru"
+)
