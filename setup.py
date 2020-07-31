4fc3484a43755ce3a0a47952eeb2a6df684d97ea
Felix-Neko <felix-neko@list.ru>
2020-07-31 23:39:03 +0300

    v.0.1.1 Long description for PyPi added.

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
-with open("readme.md", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='argparse_dataclasses',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     author="Felix Neko",
     author_email="felix-neko@list.ru",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
