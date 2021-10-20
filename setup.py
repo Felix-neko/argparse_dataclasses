6e3d9dddcd55499de454e62e75f02ec64347bc94
Felix Neko <felix-neko@list.ru>
2021-10-20 16:33:23 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     name='argparse_dataclasses',
     version='0.1.3',
     packages=find_packages(),
+    url="https://github.com/Felix-neko/argparse_dataclasses",
     author="Felix Neko",
     author_email="felix-neko@list.ru",
     long_description=long_description,
